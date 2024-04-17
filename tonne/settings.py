90dac505fa943c5f2fedc01e457bbe1b00c77779
Benjamin Schäfer <benibschafer@gmail.com>
2024-04-17 20:20:45 +0000

    feat: move secret key to env.py

--- tonne/settings.py
+++ tonne/settings.py
@@ -24,7 +24,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n&sy^s%%@z7x31%hs2do%-h#8==&j&(hzn%&#im-_()k=d9rlr'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
