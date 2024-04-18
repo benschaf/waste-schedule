86c883fcc8e9bfa5d17d1dbbf97cd5af244523b5
Benjamin Schäfer <benibschafer@gmail.com>
2024-04-18 08:39:30 +0000

    feat: add new models to admin panel
    added like, comment and sucbsciption models

--- wasteschedules/admin.py
+++ wasteschedules/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Schedule, Location
+from .models import Schedule, Location, Comment, Like, Subscription
 
 # Register your models here.
 admin.site.register(Schedule)
 admin.site.register(Location)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(Subscription)
