6014893445186275b417e24d73dac7ae7b31469c
Benjamin Schäfer <benibschafer@gmail.com>
2024-04-18 06:59:14 +0000

    feat: add like, comment and subscription models

--- wasteschedules/models.py
+++ wasteschedules/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from django.forms import ValidationError
 
 # Create your models here.
+
+
 class Schedule(models.Model):
     """
     Represents a schedule for waste collection.
@@ -17,7 +19,8 @@ class Schedule(models.Model):
 
     title = models.CharField(max_length=100)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='schedules')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='schedules')
     description = models.TextField(blank=True)
     locations = models.ManyToManyField('Location', related_name='schedules')
     created_on = models.DateTimeField(auto_now_add=True)
@@ -56,8 +59,51 @@ class Location(models.Model):
         created_at (datetime): The timestamp when the location was created.
     """
 
-    postal_code = models.CharField(validators=[validate_postal_code], primary_key=True)
+    postal_code = models.CharField(
+        validators=[validate_postal_code], primary_key=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.postal_code
+
+
+class Comment(models.Model):
+    """
+    Represents a comment made by a user on a schedule.
+    """
+
+    commented_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    schedule_id = models.ForeignKey(Schedule, on_delete=models.CASCADE)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.commented_by}"
+
+
+class Like(models.Model):
+    """
+    Represents a like given by a user to a schedule.
+    """
+
+    liked_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    schedule_id = models.ForeignKey(Schedule, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Like by {self.liked_by}"
+
+class Subscription(models.Model):
+    """
+    Represents a subscription made by a user to a schedule.
+    """
+
+    subscribed_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    schedule_id = models.ForeignKey(Schedule, on_delete=models.CASCADE)
+    subscribed_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Subscription by {self.subscribed_by}"
