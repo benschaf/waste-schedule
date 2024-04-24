d5c3f700c0a9430ebc225df50322b838dd95e4e0
Benjamin Schäfer <benibschafer@gmail.com>
2024-04-24 10:44:38 +0000

    feat: export edit comment script to comments.js
    This is done for code clarity and to make the code more modular.
    There might be a vulnerability due to the passing of the csrf_token
    to the script

---
+++ static/js/comments.js
@@ -0,0 +1,18 @@
+let editButtons = document.getElementsByClassName("btn-link");
+
+for (let button of editButtons) {
+    button.addEventListener("click", (e) => {
+        const commentFieldId = e.target.dataset.commentFieldId;
+        const commentField = document.getElementById(commentFieldId);
+        const actionUrl = e.target.dataset.actionUrl;
+        const currentText = commentField.innerText;
+        const csrfToken = e.target.dataset.csrfToken;
+        // I wonder if this makes my token vulnerable ...
+        commentField.innerHTML = `
+                    <form method="POST" action="${actionUrl}">
+                        <input type="hidden" name="csrfmiddlewaretoken" value="${csrfToken}">
+                        <input type="textarea" class="form-control" name="body" value="${currentText}">
+                        <input type="submit" value="Edit Comment" class="btn btn-link">
+                    </form>`
+    });
+}
