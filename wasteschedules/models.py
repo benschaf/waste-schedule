2d808a4ef06a45ce68a82a7078e8b7ebe4a15027
Benjamin Schäfer <benibschafer@gmail.com>
2024-04-25 12:35:20 +0000

    feat: add slugify to WasteSchedule model
    this will automatically create a unique slug for each WasteSchedule
    object that can be used in the URL

--- wasteschedules/models.py
+++ wasteschedules/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.forms import ValidationError
 from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -29,6 +30,11 @@ class Schedule(models.Model):
 
     class Meta:
         ordering = ["-created_on"]
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
