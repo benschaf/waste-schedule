b2d71295222b4096483f23244f70c0f9dbb65198
Benjamin Schäfer <benibschafer@gmail.com>
2024-04-25 12:28:10 +0000

    feat: add Schedulebuilder App

--- tonne/urls.py
+++ tonne/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path('wasteschedules/', include('wasteschedules.urls')),
+    path('schedule-builder/', include('schedulebuilder.urls')),
 ]
