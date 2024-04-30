80e56d3d38b120154ddd1793e378855a9d2f70c0
Benjamin Schäfer <benibschafer@gmail.com>
2024-04-30 09:07:17 +0000

    refac: delete unused django forms
    these forms got replaced by FullCalendar

--- schedulebuilder/forms.py
+++ schedulebuilder/forms.py
@@ -6,14 +6,3 @@ class ScheduleForm(forms.ModelForm):
     class Meta:
         model = Schedule
         fields = ('title', 'description', 'locations')
-
-class EventForm(forms.ModelForm):
-    class Meta:
-        model = Event
-        fields = ('kind', 'date', 'is_recurring', 'recurring_type', 'separation_count', 'max_number_of_occurrences')
-
-
-class ExceptionForm(forms.ModelForm):
-    class Meta:
-        model = Exception
-        fields = ('old_date', 'new_date', 'is_cancelled')
