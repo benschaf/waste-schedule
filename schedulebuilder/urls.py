eb27a0c7e11e28254c788c561fe4708445ca8fb6
Benjamin Schäfer <benibschafer@gmail.com>
2024-04-30 09:10:46 +0000

    feat: add schedule creation with fullCalendar
    this is a larger commit that adds the ability to create schedules
    using the fullCalendar library. This commit also includes a new
    view to handle the schedule creation and a new template to display
    the calendar.

--- schedulebuilder/urls.py
+++ schedulebuilder/urls.py
@@ -3,8 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.CreateSchedule.as_view(), name='create_schedule'),
-    path('add-bin/<int:schedule_id>', views.add_bin, name='add_bin'),
-    path('add-exceptions/<int:schedule_id>', views.AddException.as_view(), name='add_exception'),
-    path('calendar', views.CalendarView.as_view(), name='calendar'),
-    path('update-or-create-events/', views.UpdateOrCreateEvents.as_view(), name='update_or_create'),
+    path('add-bins/<int:schedule_id>', views.CalendarView.as_view(), name='add_bins'),
 ]
--- schedulebuilder/views.py
+++ schedulebuilder/views.py
@@ -10,11 +10,17 @@ from django.core.exceptions import PermissionDenied
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+
 # Create your views here.
 
 
-class CreateSchedule(CreateView, LoginRequiredMixin):
+class CreateSchedule(LoginRequiredMixin, CreateView):
+    login_url = reverse_lazy('account_login')
+    redirect_field_name = None
     form_class = ScheduleForm
+    template_name = 'schedulebuilder/schedule_form.html'
     model = Schedule
 
     def form_valid(self, form):
@@ -22,74 +28,57 @@ class CreateSchedule(CreateView, LoginRequiredMixin):
         return super().form_valid(form)
 
     def get_success_url(self):
-        return reverse('add_bin', kwargs={'schedule_id': self.object.id})
+        return reverse('add_bins', kwargs={'schedule_id': self.object.id})
 
     # to add the location i will need an inline formset...
 
 
-@login_required
-def add_bin(request, schedule_id):
-    schedule_obj = get_object_or_404(Schedule, pk=schedule_id)
-
-    if request.user != schedule_obj.author:
-        raise PermissionDenied("You are not the author of this schedule.")
-
-    if request.method == 'POST':
-        form = EventForm(request.POST)
-        if form.is_valid():
-            form.instance.schedule_id = schedule_obj
-            if not form.instance.is_recurring:
-                form.instance.recurring_type = None
-                form.instance.separation_count = None
-                form.instance.max_number_of_occurrences = None
-            else:
-                if form.instance.recurring_type is None or form.instance.separation_count is None or form.instance.max_number_of_occurrences is None:
-                    form.add_error(
-                        None, "Please provide values for recurring type, separation count, and max number of occurrences.")
-                    events = Event.objects.filter(schedule_id=schedule_id)
-                    return redirect(reverse('add_bin', kwargs={'schedule_id': schedule_id}))
-            form.save()
-            if 'add_another' in request.POST:
-                return redirect(reverse('add_bin', kwargs={'schedule_id': schedule_id}))
-            elif 'continue' in request.POST:
-                return redirect(reverse('add_exception', kwargs={'schedule_id': schedule_id}))
-
-    form = EventForm()
-    events = Event.objects.filter(schedule_id=schedule_id)
-    return render(request, 'schedulebuilder/event_form.html', {
-        'form': form,
-        'events': events,
-    })
-
-
-class AddException(CreateView, LoginRequiredMixin):
-    form_class = ExceptionForm
-    model = Exception
-    success_url = reverse_lazy('create_schedule')
-
-    def form_valid(self, form):
-        # need to find a way to pass the event id into this view - i think i want to display an overview calendar before this view
-        form.instance.event_id = Event.objects.last()
-        return super().form_valid(form)
-
-
-class CalendarView(TemplateView):
+# maybe integrate this with the update or create events view - so the form can be displayed and post data using the same view
+class CalendarView(LoginRequiredMixin, TemplateView):
+    login_url = reverse_lazy('account_login')
+    redirect_field_name = None
     template_name = 'schedulebuilder/calendar.html'
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, schedule_id, **kwargs):
         context = super().get_context_data(**kwargs)
-        #change the hardcoded schedule id
-        # context["schedule_id"] = get_object_or_404(Schedule, id=10)
+        context["schedule"] = get_object_or_404(Schedule, id=schedule_id)
         return context
 
+    def _convert_kind_to_int(self, kind):
+        if kind == 'restmuell':
+            return 0
+        elif kind == 'biomuell':
+            return 1
+        elif kind == 'papiermuell':
+            return 2
+        elif kind == 'gelbe-tonne':
+            return 4
+        else:
+            return None
 
+    def post(self, request, schedule_id):
+        schedule = get_object_or_404(Schedule, id=schedule_id)
+        print(request.body)
 
-class UpdateOrCreateEvents(View, LoginRequiredMixin):
-    def post(self, request):
         try:
             data = json.loads(request.body)
-            # the data is good now i first need to figure out which schedule to add the events to
-            # for this i need to figure out how to structure the new form that uses the calendar
+            # -> Credit for update_or_create: https://docs.djangoproject.com/en/5.0/ref/models/querysets/#update-or-create
+            # -> Credit for dict.get(): https://www.w3schools.com/python/ref_dictionary_get.asp
+            for event_data in data:
+                kind = self._convert_kind_to_int(event_data['title'])
+                print(event_data)
+                event, created = Event.objects.update_or_create(
+                    js_event_id=event_data['id'],
+                    schedule_id = schedule,
+                    defaults={
+                        'kind': kind,
+                        'date': event_data['start'],
+                        'recurring_type': event_data.get('rrule', {}).get('freq', None),
+                        'separation_count': event_data.get('rrule', {}).get('interval', None),
+                        'rrule_start': event_data.get('rrule', {}).get('dstart', None),
+                        'until': event_data.get('rrule', {}).get('until', None),
+                    }
+                )
             # oh and also i need to find out how to add exceptions to the rrule data
             return JsonResponse({'message': 'Success'}, status=200)
         except json.JSONDecodeError:
