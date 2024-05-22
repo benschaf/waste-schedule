8be6b7f39126625d91502bf598d49e48277701ac
Benjamin Schäfer <benibschafer@gmail.com>
2024-05-22 08:20:39 +0000

    fix: add missing " to index.html

---
+++ staticfiles/js/index.js
@@ -0,0 +1,8 @@
+// -> Credit for redirecting to a url in javascript: https://stackoverflow.com/questions/503093/how-can-i-make-a-redirect-page-in-jquery-javascript
+document.getElementById('postcodeForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    var postcode = document.getElementById('postcodeInput').value;
+    let url = window.location.href;
+    url = url.substring(0, url.lastIndexOf('wasteschedules/')) + 'wasteschedules/location/' + postcode + '/';
+    window.location.href = url;
+});
