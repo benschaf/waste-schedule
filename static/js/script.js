642a2ebae799e3500885874b99dd83727a83b350
Benjamin Schäfer <benibschafer@gmail.com>
2024-05-22 13:16:00 +0000

    feat: add navbar opacity transition on scroll

--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,24 @@
 // -> Credit for popovers: https://getbootstrap.com/docs/5.2/components/popovers/#overview
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+window.addEventListener('scroll', (event) => {
+    if (window.scrollY > 0) {
+        console.log('scrolling');
+        document.getElementsByTagName('nav')[0].classList.remove('bg-transparent');
+    } else {
+        if (!document.getElementsByTagName('nav')[0].classList.contains('bg-transparent')) {
+            document.getElementsByTagName('nav')[0].classList.add('bg-transparent');
+        }
+    }
+});
+
+document.getElementsByClassName('navbar-toggler')[0].addEventListener('click', (event) => {
+    if (window.scrollY == 0) {
+        if (!document.getElementsByTagName('nav')[0].classList.contains('bg-transparent')) {
+            document.getElementsByTagName('nav')[0].classList.add('bg-transparent');
+        } else {
+            document.getElementsByTagName('nav')[0].classList.remove('bg-transparent');
+        }
+    }
+});
