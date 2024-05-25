12339814de59d04e1c1f3bf097a556016b93c425
Benjamin Schäfer <benibschafer@gmail.com>
2024-05-25 06:38:42 +0000

    feat: style comment section

--- wasteschedules/forms.py
+++ wasteschedules/forms.py
@@ -1,8 +1,10 @@
 from .models import Comment
 from django import forms
 
-
 class CommentForm(forms.ModelForm):
+    # -> Credit for changing form widgets: https://docs.djangoproject.com/en/5.0/ref/forms/widgets/
+    body = forms.CharField(label='Your comment:', widget=forms.Textarea(attrs={'rows': 4}))
+
     class Meta:
         model = Comment
         fields = ('body',)
