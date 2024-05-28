4afb04861aea9146c533823726d68496f7040023
Benjamin Schäfer <benibschafer@gmail.com>
2024-05-28 07:58:34 +0000

    feat: add about page

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.LandingPage.as_view(), name='landing_page'),
+    path('about/', views.AboutPage.as_view(), name='about_page'),
 ]
--- core/views.py
+++ core/views.py
@@ -5,3 +5,6 @@ from django.views.generic import TemplateView
 
 class LandingPage(TemplateView):
     template_name = 'core/index.html'
+
+class AboutPage(TemplateView):
+    template_name = 'core/about.html'
